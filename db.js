a41f2374c31219a71a9be730956fbddb7ae4b5c5
EC2 Default User <ec2-user@ip-172-31-5-253.us-east-2.compute.internal>
2019-09-20 17:21:03 +0000

    generate Id's function

--- db.js
+++ db.js
@@ -5,3 +5,10 @@ const { Client } = pg;
 const client = new Client('postgres://localhost/acme_post_tags_db');
 
 client.connect();
+
+const generateIds = (...names)=> {
+  return names.reduce((acc, name)=> {
+    acc[name] = uuid.v4();
+    return acc;
+  }, {});
+};
